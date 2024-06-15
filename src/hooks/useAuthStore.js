a89ae8e8b55f6db79020bbd0d2deee38697d3555
Heyner Leiva <heynerleiva96@gmail.com>
2024-06-15 10:11:28 -0600

    Bug en el checkAuthToken solucionado.

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -58,8 +58,7 @@ export const useAuthStore = () => {
 
     try {
 
-      const { data } = calendarApi.get('/auth/renew');
-      console.log({data});
+      const { data } = await calendarApi.get('/auth/renew');
       localStorage.setItem('token', data.token);
       localStorage.setItem('token-init-date', new Date().getTime());
       dispatch(onLogin({ name: data.name, uid: data.uid }));
